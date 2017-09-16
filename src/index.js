ad118f0c169570ca3ec892655044bf88c1a35f8d
Sergi Martínez <sergi.martinez@gmail.com>
2017-09-17 00:40:20 +0200

    plaing with ports

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer((req, res) => {
   res.writeHead(200, {"Content-Type":"text/plain"});
-  res.end("hellooooo");
-}).listen(8080);
+  res.end("hellooooo from " + req.connection.localPort + " to " + req.connection.remotePort);
+}).listen(process.env.PORT || 8080);
 
 console.log("hello world");
