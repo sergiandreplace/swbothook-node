003e8fff4972fd3558a28f9444c5d3ef72754279
Sergi Martínez <sergi.martinez@scmspain.com>
2018-04-13 08:44:22 +0200

    server now starts ngrok by itself

--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
 var express = require('express');
 var action = require('./action');
 var bodyParser = require('body-parser');
+const ngrok = require('ngrok');
 
 const app = express()
 app.use(bodyParser.json());
 
-app.post("/",   function(req, res)  {
+app.post("/", function (req, res) {
 
-    body = req.body
+  body = req.body
 
-    action.process(body)
-    .then(function(response) { 
+  action.process(body)
+    .then(function (response) {
       res.statusCode = 200
       res.setHeader('Content-Type', 'application/json')
       res.send(response)
     },
-    function(error) {
-      console.log(error)
-    })
+      function (error) {
+        console.log(error)
+      })
 })
 
 app.listen(process.env.PORT || 8080);;
 
+ngrok.connect(8080)
+  .then(url => console.log(`Running at ${url}\nCheck traffic at http://127.0.0.1:4040/`))
